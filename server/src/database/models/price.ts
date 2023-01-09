2818c7c945e506a156f38ca58b599354ece9d8d6
StephenLHChan <stephenc.1018@gmail.com>
2023-01-09 17:33:03 -0500

    add price models

---
+++ server/src/database/models/price.ts
@@ -0,0 +1,26 @@
+import Sequelize from '../database';
+import { DataTypes } from 'sequelize';
+
+
+const Price = Sequelize.define('price', {
+  price: {
+    type: DataTypes.FLOAT,
+    allowNull: false
+  },
+  productId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'products',
+      key: 'id'
+    }
+  },
+  merchantId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'merchants',
+      key: 'id'
+    }
+  }
+});
+
+export default Price;
