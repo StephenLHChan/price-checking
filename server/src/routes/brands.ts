0fe8a09f8a56be1d0fcfe12e31b8283a4f302884
StephenLHChan <stephenc.1018@gmail.com>
2023-01-09 23:28:54 -0500

    add validation middleware for brand

--- server/src/routes/brands.ts
+++ server/src/routes/brands.ts
@@ -1,8 +1,23 @@
 import express from 'express';
 import { Brand } from '../database/models';
+import * as Joi from 'joi';
 
 const router = express.Router();
 
+// Validation middleware
+const validateBrand = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    const schema = Joi.object({
+      name: Joi.string().min(1).required(),
+    });
+    const { error } = schema.validate(req.body);
+    if (error) {
+      res.status(400).json({ error: error.message });
+      return;
+    }
+    next();
+  };
+
+// To get all brands
 router.get('/', async (req: express.Request, res: express.Response) => {
     const brands = await Brand.findAll();
     res.json(brands);
