2e1d10b19cd4f0b957a95bf190c35ccd472b993d
StephenLHChan <stephenc.1018@gmail.com>
2023-01-09 22:35:18 -0500

    add db relationship

--- server/src/database/models/models.ts
+++ server/src/database/models/models.ts
@@ -1,6 +1,13 @@
 import Brand from './brand';
 import Product from './product';
+import ProductCategory from './productCategory';
+import ProductSubCategory from './productSubCategory';
 
+
+ProductCategory.hasMany(ProductSubCategory);
+ProductSubCategory.belongsTo(ProductCategory);
+ProductSubCategory.hasMany(Product);
+Product.belongsTo(ProductSubCategory);
 
 Product.belongsTo(Brand);
 Brand.hasMany(Product);
