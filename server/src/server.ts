7076576a82a0ce8475a84903a478b7284fa66fdb
StephenLHChan <stephenc.1018@gmail.com>
2023-01-14 10:23:24 -0500

    add routes to server

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import 'express-async-errors';
 
+import routes from './routes';
+
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(routes);
 
 const errorHandling = (err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
   res.status(500).json({
