2665edc410122b32d0a32c9b5e4fa690f56116ce
StephenLHChan <stephenc.1018@gmail.com>
2023-01-18 21:21:17 -0500

    refactor validation middleware

--- server/src/routes/middleware/validation.ts
+++ server/src/routes/middleware/validation.ts
@@ -1,28 +1,33 @@
 import express from 'express';
 import * as Joi from 'joi';
 
+
+const validate = (schema: any, req: express.Request, res: express.Response) => {
+  const { error } = schema.validate(req.body);
+  if (error) {
+    res.status(400).json({ error: error.message });
+    return false;
+  }
+  return true;
+}
+
+
 const validateBrand = (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const schema = Joi.object({
       name: Joi.string().min(1).required(),
     });
-    const { error } = schema.validate(req.body);
-    if (error) {
-      res.status(400).json({ error: error.message });
-      return;
-    }
-    next();
+    if (validate(schema, req, res)) {
+      next();
+   }
   };
 
 const validateMerchant = (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const schema = Joi.object({
       name: Joi.string().min(1).required(),
     });
-    const { error } = schema.validate(req.body);
-    if (error) {
-      res.status(400).json({ error: error.message });
-      return;
-    }
-    next();
+    if (validate(schema, req, res)) {
+      next();
+   }
   };
 
   const validatePrice = (req: express.Request, res: express.Response, next: express.NextFunction) => {
@@ -31,12 +36,9 @@ const validateMerchant = (req: express.Request, res: express.Response, next: exp
       productId: Joi.number().integer().required(),
       merchantId: Joi.number().integer().required()
     });
-    const { error } = schema.validate(req.body);
-    if (error) {
-      res.status(400).json({ error: error.message });
-      return;
-    }
-    next();
+    if (validate(schema, req, res)) {
+      next();
+   }
   };
 
   const validateProduct = (req: express.Request, res: express.Response, next: express.NextFunction) => {
@@ -45,24 +47,18 @@ const validateMerchant = (req: express.Request, res: express.Response, next: exp
       subCategoryId: Joi.number().required(),
       brandId: Joi.number().required()
     });
-    const { error } = schema.validate(req.body);
-    if (error) {
-      res.status(400).json({ error: error.message });
-      return;
-    }
-    next();
+    if (validate(schema, req, res)) {
+      next();
+   }
   };
 
   const validateProductCategory = (req: express.Request, res: express.Response, next: express.NextFunction) => {
     const schema = Joi.object({
       name: Joi.string().min(1).required(),
     });
-    const { error } = schema.validate(req.body);
-    if (error) {
-      res.status(400).json({ error: error.message });
-      return;
-    }
-    next();
+    if (validate(schema, req, res)) {
+      next();
+   }
   };
 
   const validateProductSubCategory = (req: express.Request, res: express.Response, next: express.NextFunction) => {
@@ -70,12 +66,9 @@ const validateMerchant = (req: express.Request, res: express.Response, next: exp
       name: Joi.string().min(1).required(),
       categoryId: Joi.number().integer().required(),
     });
-    const { error } = schema.validate(req.body);
-    if (error) {
-      res.status(400).json({ error: error.message });
-      return;
-    }
-    next();
+    if (validate(schema, req, res)) {
+      next();
+   }
   };
 
 export {validateBrand, validateMerchant, validatePrice, validateProduct, validateProductCategory, validateProductSubCategory};
