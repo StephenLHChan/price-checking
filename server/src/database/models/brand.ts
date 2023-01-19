82ceda2a1baf60c6e9a6bd1ce9cc9f2fcb44f8c2
StephenLHChan <stephenc.1018@gmail.com>
2023-01-18 23:59:38 -0500

    add fields to brands model

--- server/src/database/models/brand.ts
+++ server/src/database/models/brand.ts
@@ -1,13 +1,24 @@
 import Sequelize from '../database';
 import { DataTypes } from 'sequelize';
 
-
 const Brand = Sequelize.define('brand', {
   name: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique: true
-  }
+    unique: true,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  website: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  logo: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
 });
 
 export default Brand;
--- server/src/routes/middleware/validation.ts
+++ server/src/routes/middleware/validation.ts
@@ -1,18 +1,19 @@
 import express from 'express';
 import * as Joi from 'joi';
 
-
 const validate = (schema: any) => (req: express.Request, res: express.Response, next: express.NextFunction) => {
   const { error } = schema.validate(req.body);
   if (error) {
     return res.status(400).json({ error: error.message });
   }
   next();
-}
-
+};
 
 const brandSchema = Joi.object({
   name: Joi.string().min(1).required(),
+  description: Joi.string().allow('').optional(),
+  website: Joi.string().allow('').optional(),
+  logo: Joi.string().allow('').optional(),
 });
 
 const merchantSchema = Joi.object({
@@ -22,13 +23,13 @@ const merchantSchema = Joi.object({
 const priceSchema = Joi.object({
   price: Joi.number().positive().required(),
   productId: Joi.number().integer().required(),
-  merchantId: Joi.number().integer().required()
+  merchantId: Joi.number().integer().required(),
 });
 
 const productSchema = Joi.object({
   name: Joi.string().min(1).required(),
   subCategoryId: Joi.number().integer().required(),
-  brandId: Joi.number().integer().required()
+  brandId: Joi.number().integer().required(),
 });
 
 const productCategorySchema = Joi.object({
@@ -40,5 +41,12 @@ const productSubCategorySchema = Joi.object({
   categoryId: Joi.number().integer().required(),
 });
 
-
-export {brandSchema, merchantSchema, priceSchema, productSchema, productCategorySchema, productSubCategorySchema, validate};
+export {
+  validate,
+  brandSchema,
+  merchantSchema,
+  priceSchema,
+  productSchema,
+  productCategorySchema,
+  productSubCategorySchema,
+};
