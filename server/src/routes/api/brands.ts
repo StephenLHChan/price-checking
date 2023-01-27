2b5edb84305b31e1c9fe185eaaed878e34f82698
StephenLHChan <stephenc.1018@gmail.com>
2023-01-26 22:44:23 -0500

    update brand endpoint

--- server/src/routes/api/brands.ts
+++ server/src/routes/api/brands.ts
@@ -5,7 +5,6 @@ import { validate, brandSchema } from '../middleware/validation';
 
 const router = express.Router();
 
-
 const checkBrandExist = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
   const { id } = req.params;
   const brand = await Brand.findByPk(id);
@@ -13,42 +12,45 @@ const checkBrandExist = async (req: express.Request, res: express.Response, next
     return res.status(404).json({ error: 'Brand not found' });
   }
   (req as any).brand = brand;
-  next()
+  next();
 };
 
 // To get all brands
 router.get('/', async (req: express.Request, res: express.Response) => {
-    const brands = await Brand.findAll();
-    res.json(brands);
+  const brands = await Brand.findAll();
+  res.json(brands);
 });
 
 // To get a brand by id
-router.get('/:id',checkBrandExist, async (req: express.Request, res: express.Response) => {
+router.get('/:id', checkBrandExist, async (req: express.Request, res: express.Response) => {
   const brand = (req as any).brand;
   res.json(brand);
 });
 
 // To create a new brand
 router.post('/', validate(brandSchema), async (req: express.Request, res: express.Response) => {
-    const { name } = req.body;
-    // Check if brand name is already in use
-    const existingBrand = await Brand.findOne({ where: { name } });
-    if (existingBrand) {
-      res.status(400).json({ error: 'Brand name already in use' });
-      return;
-    }
-    // Create brand
-    const brand = await Brand.create({ name });
-    res.status(201).json(brand);
-  });
+  const { name, descriptions, website, logo } = req.body;
+  // Check if brand name is already in use
+  const existingBrand = await Brand.findOne({ where: { name } });
+  if (existingBrand) {
+    return res.status(400).json({ error: 'Brand name already in use' });
+  }
+  // Create brand
+  const brand = await Brand.create({ name, descriptions, website, logo });
+  return res.status(201).json(brand);
+});
 
 // To update the brand
 router.patch('/:id', validate(brandSchema), checkBrandExist, async (req: express.Request, res: express.Response) => {
   const brand = (req as any).brand;
-  const { name } = req.body;
-  await brand.update({ name });
+  const { name, descriptions, website, logo } = req.body;
+  const existingBrand = await Brand.findOne({ where: { name } });
+  if (existingBrand) {
+    return res.status(400).json({ error: 'Brand name already in use' });
+  }
+
+  await brand.update({ name, descriptions, website, logo });
   res.json(brand);
 });
-
 
 export default router;
