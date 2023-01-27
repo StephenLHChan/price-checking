16996789ef08f463f0bfb6d1973b8bac808a86ae
StephenLHChan <stephenc.1018@gmail.com>
2023-01-26 23:00:03 -0500

    update product model

--- server/src/database/models/product.ts
+++ server/src/database/models/product.ts
@@ -4,22 +4,30 @@ import { DataTypes } from 'sequelize';
 const Product = Sequelize.define('product', {
   name: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: true,
   },
   subCategoryId: {
     type: DataTypes.INTEGER,
     references: {
       model: 'product_sub_categories',
-      key: 'id'
-    }
+      key: 'id',
+    },
   },
   brandId: {
     type: DataTypes.INTEGER,
     references: {
       model: 'brands',
       key: 'id',
-    }
-  }
+    },
+  },
 });
 
 export default Product;
