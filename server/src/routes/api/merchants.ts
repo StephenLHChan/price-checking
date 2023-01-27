67980747791355e4f2bdf09f277dd2e39a86bbce
StephenLHChan <stephenc.1018@gmail.com>
2023-01-26 22:51:23 -0500

    update merchant endpoint

--- server/src/routes/api/merchants.ts
+++ server/src/routes/api/merchants.ts
@@ -5,7 +5,6 @@ import { validate, merchantSchema } from '../middleware/validation';
 
 const router = express.Router();
 
-
 const checkMerchantExist = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
   const { id } = req.params;
   const merchant = await Merchant.findByPk(id);
@@ -13,41 +12,50 @@ const checkMerchantExist = async (req: express.Request, res: express.Response, n
     return res.status(404).json({ error: 'Merchant not found' });
   }
   (req as any).merchant = merchant;
-  next()
+  next();
 };
 
 // To get all merchants
 router.get('/', async (req: express.Request, res: express.Response) => {
-    const merchants = await Merchant.findAll();
-    res.json(merchants);
+  const merchants = await Merchant.findAll();
+  res.json(merchants);
 });
 
-router.get('/:id',checkMerchantExist, async (req: express.Request, res: express.Response) => {
+// To get merchants by id
+router.get('/:id', checkMerchantExist, async (req: express.Request, res: express.Response) => {
   const merchant = (req as any).merchant;
   res.json(merchant);
 });
 
 // To create a new merchant
 router.post('/', validate(merchantSchema), async (req: express.Request, res: express.Response) => {
+  const { name } = req.body;
+  // Check if merchant name is already in use
+  const existingMerchant = await Merchant.findOne({ where: { name } });
+  if (existingMerchant) {
+    return res.status(400).json({ error: 'Merchant name already in use' });
+  }
+  // Create merchant
+  const merchant = await Merchant.create({ name });
+  res.status(201).json(merchant);
+});
+
+// To update the merchant
+router.patch(
+  '/:id',
+  validate(merchantSchema),
+  checkMerchantExist,
+  async (req: express.Request, res: express.Response) => {
+    const merchant = (req as any).merchant;
     const { name } = req.body;
     // Check if merchant name is already in use
     const existingMerchant = await Merchant.findOne({ where: { name } });
     if (existingMerchant) {
-      res.status(400).json({ error: 'Merchant name already in use' });
-      return;
+      return res.status(400).json({ error: 'Merchant name already in use' });
     }
-    // Create merchant
-    const merchant = await Merchant.create({ name });
-    res.status(201).json(merchant);
-  });
-
-// To update the merchant
-router.patch('/:id', validate(merchantSchema), checkMerchantExist, async (req: express.Request, res: express.Response) => {
-    const merchant = (req as any).merchant;
-    const { name } = req.body;
     await merchant.update({ name });
     res.json(merchant);
-});
-
+  }
+);
 
 export default router;
