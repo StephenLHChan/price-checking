ae19ce25e3471c1b948e27116f08f6157e7a919f
StephenLHChan <stephenc.1018@gmail.com>
2023-02-18 15:39:22 -0500

    Add view-brands to routes

--- client-web/src/routes.tsx
+++ client-web/src/routes.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import ViewBrands from './pages/ViewBrands';
 
 const RouterConfig = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/view-brands" element={<ViewBrands />} />
     </Routes>
   );
 };
