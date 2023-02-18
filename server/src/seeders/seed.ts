3596d7f6054411ac8f159e5ca96165c22c0172fb
StephenLHChan <stephenc.1018@gmail.com>
2023-02-18 15:17:11 -0500

    update seed file

--- server/src/seeders/seed.ts
+++ server/src/seeders/seed.ts
@@ -1,23 +1,28 @@
 import { faker } from '@faker-js/faker';
 
-import sequelize  from '../database/database';
+import sequelize from '../database/database';
 import Brand from '../database/models/brand';
 import Merchant from '../database/models/merchant';
 import ProductCategory from '../database/models/productCategory';
 
-const brands: Array<{ [key: string]: string }> = [{name: 'Brand 1'}];
-for(let i = 0; i < 3; i++){
-  brands.push({ name: faker.company.name()})
+const brands: Array<{ [key: string]: string }> = [{ name: 'Brand 1' }];
+for (let i = 0; i < 3; i++) {
+  brands.push({
+    name: faker.company.name(),
+    description: faker.lorem.paragraph(),
+    website: faker.internet.url(),
+    logo: faker.internet.url(),
+  });
 }
 
-const merchants: Array<{ [key: string]: string }> = [{name: 'Merchant 1'}];
-for(let i = 0; i < 3; i++){
-  merchants.push({ name: faker.company.name()})
+const merchants: Array<{ [key: string]: string }> = [{ name: 'Merchant 1' }];
+for (let i = 0; i < 3; i++) {
+  merchants.push({ name: faker.company.name() });
 }
 
-const product_categories: Array<{ [key: string]: string }> = [{name: 'Product Categories 1'}];
-for(let i = 0; i < 3; i++){
-  product_categories.push({ name: faker.commerce.department()})
+const product_categories: Array<{ [key: string]: string }> = [{ name: 'Product Categories 1' }];
+for (let i = 0; i < 3; i++) {
+  product_categories.push({ name: faker.commerce.department() });
 }
 
 (async () => {
